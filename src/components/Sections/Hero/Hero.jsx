664b348b5f72a819f116473855cd8184954d43f1
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-12-27 00:56:53 +0200

    add header and hero styles

--- src/components/Sections/Hero/Hero.jsx
+++ src/components/Sections/Hero/Hero.jsx
@@ -6,15 +6,23 @@ const Hero = () => {
   return (
     <section id="hero">
       <div className={css.heroWrapper}>
-        <h1>Test assignment for front-end developer</h1>
-        <p>
-          What defines a good front-end developer is one that has skilled
-          knowledge of HTML, CSS, JS with a vast understanding of User design
-          thinking as they'll be building web interfaces with accessibility in
-          mind. They should also be excited to learn, as the world of Front-End
-          Development keeps evolving.
-        </p>
-              <Button text="Sign Up" func={() => console.log(2)} className={css.heroBtn} />
+        <div className={css.heroContent}>
+          <h1 className={css.heroHeading}>
+            Test assignment for front-end developer
+          </h1>
+          <p className={css.heroText}>
+            What defines a good front-end developer is one that has skilled
+            knowledge of HTML, CSS, JS with a vast understanding of User design
+            thinking as they'll be building web interfaces with accessibility in
+            mind. They should also be excited to learn, as the world of
+            Front-End Development keeps evolving.
+          </p>
+          <Button
+            text="Sign Up"
+            func={() => console.log(2)}
+            className={css.heroBtn}
+          />
+        </div>
       </div>
     </section>
   );
